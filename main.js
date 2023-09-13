0b63fc0ebab020dfebcd2b7e87fc2a453f4039bf
sum117 <jvcaliman@hotmail.com>
2023-09-13 17:59:35 -0300

    bugfix: forgot to make conversion of lat and long

--- main.js
+++ main.js
@@ -96,8 +96,8 @@ function makePayload({ token, clientId, uuid, login, sessionToken }) {
             "pin": null,
         },
         "time_card": {
-            "latitude": BUN.env.LATITUDE,
-            "longitude": BUN.env.LONGITUDE,
+            "latitude": parseInt(BUN.env.LATITUDE),
+            "longitude": parseInt(BUN.env.LONGITUDE),
             "address": Bun.env.ADDRESS,
             "original_latitude": null,
             "original_longitude": null,
