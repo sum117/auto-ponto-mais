0087055863db5a83b64d0eb1d6ca991ff0891c9c
sum117 <jvcaliman@hotmail.com>
2023-09-14 13:42:51 -0300

    ci: update script

--- update.sh
+++ update.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
 docker build --no-cache -t sum117/auto-ponto-mais .
 docker push sum117/auto-ponto-mais
+
+while getopts "m:" flag; do
+    case "${flag}" in
+        m) message=${OPTARG} ;;
+        *) echo "Uso inválido: -$OPTARG" >&2
+           exit 1 ;;
+    esac
+done
+
+git add .
+git commit -am "$message"
+git push
